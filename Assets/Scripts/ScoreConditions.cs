c976f28822ce157e53e6c341f4600ae47b1e4ba3
BrandedTales <lordbrand@gmail.com>
2020-07-23 12:08:54 -0500

    2.  Sorted out the ScoreConditions as a serializable class.  Added logic to show a text version of the score conditions.  Also added logic for the resource reward display.  Adjusted some enums.

---
+++ Assets/Scripts/ScoreConditions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BT.CrystalBright 
+{
+    public enum CardType { None, Martial, Magic, Creature, Any }
+    public enum ScoreConditionType { None, Score, Required, Restricted }
+    public enum ScoreConditionDirection { TopLeft, Top, TopRight, Left, Right, BottomLeft, Bottom, BottomRight }
+    public enum ScoreConditionDistance { Self, One, Two, Both }
+    public enum CardRewardType { OneRed, OneGreen, OneBlue }
+
+  
+    [CreateAssetMenu(fileName = "New Score Condition", menuName = "CrystalBright/Score Conditions", order = 1)]
+    public class ScoreConditions : ScriptableObject 
+    {
+        public ScoreConditionType scoreConditionType;
+        public ScoreConditionDirection scoreConditionDirection;
+        public ScoreConditionDistance scoreConditionDistance;
+        public CardType cardType;
+    }
+}
