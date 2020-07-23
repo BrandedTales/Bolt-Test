a7489b3f5d938b2ca6261260356c9a125e81cfc3
BrandedTales <lordbrand@gmail.com>
2020-07-23 00:42:52 -0500

    0. Setup

---
+++ Assets/Scripts/CardEnums.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BT.CrystalBright 
+{
+    public class CardEnums : MonoBehaviour
+    {
+        public enum CardType { None, Martial, Magic, Creature }
+        public enum ScoreConditionType { None, Score, Required, Restricted }
+    }
+}
