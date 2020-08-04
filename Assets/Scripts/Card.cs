e629d393f10ef96d863532253005b404b32bb9ea
BrandedTales <lordbrand@gmail.com>
2020-08-03 23:41:07 -0500

    21.  Created the swapped card variable, but doesn't seem to be tracking.  Added logic for scriptable objects to cover card appearances.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BT.CrystalBright 
+{
+    [CreateAssetMenu(fileName = "Card", menuName="CrystalBright/New Card", order = 1)]
+    public class Card : ScriptableObject
+    {
+        public Sprite cardImage;
+        public CardType cardType;
+        public CardRewardType cardRewardType;
+        
+    }
+}
